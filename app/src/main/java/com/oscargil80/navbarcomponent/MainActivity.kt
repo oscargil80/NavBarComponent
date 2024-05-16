393a8f3dcdfc5029ffb4270afd3726e3f8feb2bc
Oscargil80 <oscargil80@gmail.com>
2024-05-15 21:22:55 -0400

    primera

--- app/src/main/java/com/oscargil80/navbarcomponent/MainActivity.kt
+++ app/src/main/java/com/oscargil80/navbarcomponent/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.oscargil80.navbarcomponent.R.*
 import com.oscargil80.navbarcomponent.databinding.ActivityMainBinding
-import com.oscargil80.navbarcomponent.bottomNavigationView as bottomNavigationView1
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,52 +14,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
         val favoritoFragment = FavoritoFragment()
         val musicaFragment = MusicaFragment()
         val notificacionFragment = NotificacionFragment()
 
         binding.bottomNavigationView.setOnNavigationItemSelectedListener {
-            when (it.itemId){
-                R.id.nav_fav ->  {
+            when (it.itemId) {
+                R.id.nav_fav -> {
                     setCurrentFragmnent(favoritoFragment)
                     true
                 }
-
-                R.id.nav_music ->  {
+                R.id.nav_music -> {
                     setCurrentFragmnent(musicaFragment)
                     true
                 }
-
-                R.id.nav_noti ->  {
+                R.id.nav_noti -> {
                     setCurrentFragmnent(notificacionFragment)
                     true
                 }
                 else -> false
+            }
         }
 
+        binding.bottomNavigationView.getOrCreateBadge(R.id.nav_noti).apply {
+            isVisible = true
+            number = 10
         }
-
-
-
     }
 
-
-
-
-
-    private fun setCurrentFragmnent(fragment:Fragment){
+    private fun setCurrentFragmnent(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
             replace(id.containerView, fragment)
             commit()
         }
     }
-
-
-}
-
-object bottomNavigationView {
-
 }
